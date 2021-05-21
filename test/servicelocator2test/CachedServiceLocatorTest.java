bcd44dae495815c44d65bdfb25caea20603afc4c
Xavier <xaavier@protonmail.com>
2021-05-21 14:53:39 +0200

    Add init test function

--- test/servicelocator2test/CachedServiceLocatorTest.java
+++ test/servicelocator2test/CachedServiceLocatorTest.java
@@ -1,4 +1,38 @@
 package servicelocator2test;
 
+import factories2.FactoryA1;
+import factories2.FactoryB1;
+import factories2.FactoryC1;
+import factories2.FactoryD1;
+import interfaces.InterfaceA;
+import interfaces.InterfaceB;
+import interfaces.InterfaceC;
+import interfaces.InterfaceD;
+import org.junit.jupiter.api.BeforeEach;
+import servicelocator2.ServiceLocator;
+import servicelocator2.CachedServiceLocator;
+import servicelocator2.LocatorError;
+
 public class CachedServiceLocatorTest {
+    ServiceLocator sl;
+    FactoryA1 factoryA;
+    FactoryB1 factoryB;
+    FactoryC1 factoryC;
+    FactoryD1 factoryD;
+
+    @BeforeEach
+    public void init() throws LocatorError {
+        sl = new CachedServiceLocator();
+        factoryA = new FactoryA1();
+        factoryB = new FactoryB1();
+        factoryC = new FactoryC1();
+        factoryD = new FactoryD1();
+
+        sl.setService(InterfaceA.class, factoryA);
+        sl.setService(InterfaceB.class, factoryB);
+        sl.setService(InterfaceC.class, factoryC);
+        sl.setService(InterfaceD.class, factoryD);
+        sl.setConstant(String.class, "string");
+        sl.setConstant(int.class, 1);
+    }
 }
--- test/servicelocatortest/SimpleServiceLocatorTest.java
+++ test/servicelocatortest/SimpleServiceLocatorTest.java
@@ -1,4 +1,34 @@
 package servicelocatortest;
 
+import factories.FactoryA1;
+import factories.FactoryB1;
+import factories.FactoryC1;
+import factories.FactoryD1;
+import org.junit.jupiter.api.BeforeEach;
+import servicelocator.LocatorError;
+import servicelocator.ServiceLocator;
+import servicelocator.SimpleServiceLocator;
+
 public class SimpleServiceLocatorTest {
+    ServiceLocator sl;
+    FactoryA1 factoryA;
+    FactoryB1 factoryB;
+    FactoryC1 factoryC;
+    FactoryD1 factoryD;
+
+    @BeforeEach
+    public void init() throws LocatorError {
+        sl = new SimpleServiceLocator();
+        factoryA = new FactoryA1();
+        factoryB = new FactoryB1();
+        factoryC = new FactoryC1();
+        factoryD = new FactoryD1();
+
+        sl.setService("A", factoryA);
+        sl.setService("B", factoryB);
+        sl.setService("C", factoryC);
+        sl.setService("D", factoryD);
+        sl.setConstant("S", "string");
+        sl.setConstant("I", 1);
+    }
 }
