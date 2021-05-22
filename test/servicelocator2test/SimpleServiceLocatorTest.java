f7aee8184c595838ed94521ae65d7e942e6fcf09
Saber Tiguer <sabertiguer@gmail.com>
2021-05-22 20:57:36 +0200

    Tests service locator segona versió

--- test/servicelocator2test/SimpleServiceLocatorTest.java
+++ test/servicelocator2test/SimpleServiceLocatorTest.java
@@ -9,9 +9,13 @@ import interfaces.InterfaceB;
 import interfaces.InterfaceC;
 import interfaces.InterfaceD;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import servicelocator2.LocatorError;
 import servicelocator2.ServiceLocator;
 import servicelocator2.SimpleServiceLocator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SimpleServiceLocatorTest {
     ServiceLocator sl;
@@ -34,5 +38,52 @@ public class SimpleServiceLocatorTest {
         sl.setService(InterfaceD.class, factoryD);
         sl.setConstant(String.class, "string");
         sl.setConstant(int.class, 1);
+    }
+
+    @Test
+    public void setService(){
+        String expectedMessage = "Ja hi ha una factoria enregistrada amb aquest nom!";
+
+        // Llença excepció
+        Exception exception = assertThrows(LocatorError.class, () -> {
+            sl.setService(InterfaceA.class, factoryA);
+        });
+        assertEquals(expectedMessage, exception.getMessage());
+
+        exception = assertThrows(LocatorError.class, () -> {
+            sl.setService(InterfaceB.class, factoryA);
+        });
+        assertEquals(expectedMessage, exception.getMessage());
+
+        exception = assertThrows(LocatorError.class, () -> {
+            sl.setService(InterfaceC.class, factoryA);
+        });
+        assertEquals(expectedMessage, exception.getMessage());
+
+        exception = assertThrows(LocatorError.class, () -> {
+            sl.setService(InterfaceD.class, factoryA);
+        });
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    @Test
+    public void setConstant(){
+        String expectedMessage = "Ja hi ha un objecte enregistrat amb aquest nom!";
+
+        // Llença excepció
+        Exception exception = assertThrows(LocatorError.class, () -> {
+            sl.setConstant(String.class, "A");
+        });
+        assertEquals(expectedMessage, exception.getMessage());
+
+        exception = assertThrows(LocatorError.class, () -> {
+            sl.setConstant(int.class, 1);
+        });
+    }
+
+    @Test
+    public void getObject() throws LocatorError {
+        assertEquals("string", sl.getObject(String.class));
+        assertEquals(1, sl.getObject(int.class));
     }
 }
